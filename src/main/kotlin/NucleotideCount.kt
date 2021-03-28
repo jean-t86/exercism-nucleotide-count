79ef3b3d1011efff26560002896a8ff9e0d030e7
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-28 08:58:34 +0400

    Add completed solution

--- src/main/kotlin/NucleotideCount.kt
+++ src/main/kotlin/NucleotideCount.kt
@@ -1,9 +1,24 @@
-class Dna {
+class Dna(private val dnaStrand: String) {
 
-    // TODO: Implement proper constructor
+    private val _nucleotideCounts: MutableMap<Char, Int> =
+        mutableMapOf('A' to 0, 'C' to 0, 'G' to 0, 'T' to 0)
+
+    init {
+        if (dnaStrand.any { it !in listOf('A', 'C', 'G', 'T') }) {
+            throw IllegalArgumentException()
+        }
+    }
 
     val nucleotideCounts: Map<Char, Int>
         get() {
-            TODO("Implement this function to complete the task")
+            if (dnaStrand.isEmpty()) {
+                return _nucleotideCounts
+            }
+
+            _nucleotideCounts.forEach { (key, value) ->
+                _nucleotideCounts.set(key, dnaStrand.count { it == key })
+            }
+
+            return _nucleotideCounts
         }
 }
